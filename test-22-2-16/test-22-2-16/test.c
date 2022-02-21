9739cfa919ad58949e20872c5538ca98d3d9f0e1
Chen-zl-cn <1187954442@qq.com>
2022-02-21 22:18:36 +0800

    日常代码

--- test-22-2-16/test-22-2-16/test.c
+++ test-22-2-16/test-22-2-16/test.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 
-//用冒泡排序实现对整型数组的排序
-
-void bubble_sort(int arr[], int sz)
-{
-	int i = 0;
-	int j = 0;
-	for (j = 0; j < sz - 1; j++)
-	{
-		for (i = 0; i < sz - 1 - j; i++)
-		{
-			if (arr[i] > arr[i + 1])
-			{
-				int tmp = arr[i + 1];
-				arr[i + 1] = arr[i];
-				arr[i] = tmp;
-			}
-		}
-	}
-}
+////用冒泡排序实现对整型数组的排序
+//
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (j = 0; j < sz - 1; j++)
+//	{
+//		for (i = 0; i < sz - 1 - j; i++)
+//		{
+//			if (arr[i] > arr[i + 1])
+//			{
+//				int tmp = arr[i + 1];
+//				arr[i + 1] = arr[i];
+//				arr[i] = tmp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 15,3,2,4,6,5,7,9,8,10,-1 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);//计算数组中元素个数
+//	bubble_sort(arr, sz);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)//打印数组
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
 
 int main()
 {
-	int arr[] = { 15,3,2,4,6,5,7,9,8,10,-1 };
-	int sz = sizeof(arr) / sizeof(arr[0]);//计算数组中元素个数
-	bubble_sort(arr, sz);
+	int arr[] = { 0 };
 	int i = 0;
-	for (i = 0; i < sz; i++)//打印数组
+	for (i = 0; i < 10; i++)
+	{
+		arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d ", arr[i]);
 	}
---
+++ test-22-2-21/test-22-2-21/test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+//´òÓ¡Ñî»ÔÈı½Ç
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i][0] = 1;
+//		arr[i][i] = 1;
+//	}
+//	for (i = 2; i < 10; i++)
+//	{
+//		for (j = 1; j < i; j++)
+//		{
+//			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//		}
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j < i+1; j++)
+//		{
+//			printf("%3d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//²ÂĞ×ÊÖ
+
+//int main()
+//{
+//	char  killer;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("%c\n", killer);
+//		}
+//	}
+//	return 0;
+//}
